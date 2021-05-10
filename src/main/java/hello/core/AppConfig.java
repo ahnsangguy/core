fe3a490683876d6507c2a4d0a38e9ca4f789f7c2
name <ahnsangguy@naver.com>
2021-05-11 00:28:30 +0900

    20210511 Home

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,22 +18,25 @@ public class AppConfig {
 	@Bean
 	public MemberService memberService() {
 		
+		System.out.println("call AppConfig.memberService()");
 		return new MemberServiceImpl(memberRepository());
 	}
 	
 	@Bean
 	public OrderService orderService() {
 		
+		System.out.println("call AppConfig.orderService()");
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 	
-//	@Bean
+	@Bean
 	public MemberRepository memberRepository() {
 		
+		System.out.println("call AppConfig.memberRepository()");
 		return new MemoryMemberRepository();
 	}
 	
-//	@Bean
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		
 //		return new FixDiscountPolicy();
